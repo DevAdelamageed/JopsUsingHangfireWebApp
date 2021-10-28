dfbcdb184f5af2429adfbd262bdebb05200d4916
AbdelMajed Mohamed Saber <Absaber@nileuniversity.edu.local>
2021-10-28 09:28:51 +0200

    first push

---
+++ JopsUsingHangfireWebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JopsUsingHangfireWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            // BackgroundJob.Enqueue(() => SendMessage("Mego saber"));
+
+            //BackgroundJob.Schedule(()=>SendMessage("Shedual Jop After Time 2 mintiuts"), TimeSpan.FromMinutes(2));
+           
+            
+            RecurringJob.AddOrUpdate( ()=> SendMessage("RecurringJob ") ,Cron.Monthly(1));
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [ApiExplorerSettings(IgnoreApi =true)]
+        public void SendMessage(string email)
+        {
+            Console.WriteLine("Email Send ");
+        }
+    }
+}
